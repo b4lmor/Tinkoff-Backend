7e0a4b1d37bfd102547946ea49ce5bdcf0f0bffb
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-10-06 22:19:57 +0700

    improve validation ; add parameterized tests for valid & invalid cases

--- src/test/java/edu/hw1/task1/VideoLengthTest.java
+++ src/test/java/edu/hw1/task1/VideoLengthTest.java
@@ -1,64 +1,52 @@
 package edu.hw1.task1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class VideoLengthTest {
-    @Test
-    @DisplayName("Parse video length from 'mm:ss', mm > 0, ss > 0")
-    void parseVideoLength_CaseMinAndSec() {
-        int result;
-        String rawVideoLength;
 
-        rawVideoLength = "10:05";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(10 * 60 + 5);
-
-        rawVideoLength = "55:11";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(55 * 60 + 11);
+    @ParameterizedTest
+    @DisplayName("Parse video length from VALID 'mm:ss'")
+    @MethodSource("provideValidParameters")
+    public void testParseValidVideoLength(String rawVideoLength, int expectedResult) {
+        final int parsedVideoLength = VideoLength.parseVideoLength(rawVideoLength);
+        assertThat(parsedVideoLength)
+            .isEqualTo(expectedResult);
     }
 
-    @Test
-    @DisplayName("Parse video length from 'mm:ss', mm = 0, ss > 0")
-    void parseVideoLength_CaseSec() {
-        int result;
-        String rawVideoLength;
-
-        rawVideoLength = "00:08";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(8);
-
-        rawVideoLength = "00:22";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(22);
+    private static Stream<Arguments> provideValidParameters() {
+        return Stream.of(
+            Arguments.of("15:55", 15 * 60 + 55),
+            Arguments.of("33:33", 33 * 60 + 33),
+            Arguments.of("10:00", 10 * 60),
+            Arguments.of("56:00", 56 * 60),
+            Arguments.of("00:07", 7),
+            Arguments.of("00:55", 55)
+        );
     }
 
-    @Test
-    @DisplayName("Parse video length from 'mm:ss', mm > 0, ss = 0")
-    void parseVideoLength_CaseMin() {
-        int result;
-        String rawVideoLength;
+    @ParameterizedTest
+    @DisplayName("Parse video length from INVALID 'mm:ss'")
+    @MethodSource("provideInvalidParameters")
+    public void testParseInvalidVideoLength(String rawVideoLength) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            VideoLength.parseVideoLength(rawVideoLength);
+        });
+    }
 
-        rawVideoLength = "40:00";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(40 * 60);
-
-        rawVideoLength = "33:00";
-        result = VideoLength.parseVideoLength(rawVideoLength);
-
-        assertThat(result)
-            .isEqualTo(33 * 60);
+    private static Stream<Arguments> provideInvalidParameters() {
+        return Stream.of(
+            Arguments.of("65:55"),
+            Arguments.of("773:33"),
+            Arguments.of("10:008"),
+            Arguments.of("6:00"),
+            Arguments.of("00:99"),
+            Arguments.of(":")
+        );
     }
 }
