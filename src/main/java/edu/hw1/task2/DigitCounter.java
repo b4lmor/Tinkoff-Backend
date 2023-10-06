990547c67615930b175ddcdf5e087111ad628938
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-10-06 22:41:23 +0700

    implement fucntion

--- src/main/java/edu/hw1/task2/DigitCounter.java
+++ src/main/java/edu/hw1/task2/DigitCounter.java
@@ -1,2 +1,25 @@
-package edu.hw1.task2;public class DigitCounter {
+package edu.hw1.task2;
+
+public class DigitCounter {
+
+    private static final int VALUE_OF_NUMBER_SYSTEM = 10;
+
+    private DigitCounter() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int countDigits(int number) {
+        if (number == 0) {
+            return 1;
+        }
+
+        int absNumber = Math.abs(number);
+        int digitCounter = 0;
+
+        while (absNumber != 0) {
+            absNumber /= VALUE_OF_NUMBER_SYSTEM;
+            digitCounter++;
+        }
+        return digitCounter;
+    }
 }
