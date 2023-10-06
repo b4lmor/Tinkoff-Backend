e4ad9bacbabf71438f5c194d0e31452fe95fd272
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-10-06 23:18:46 +0700

    task 3 : Nest checker + tests

---
+++ src/main/java/edu/hw1/task3/NestChecker.java
@@ -0,0 +1,32 @@
+package edu.hw1.task3;
+
+import java.util.Arrays;
+
+public class NestChecker {
+    private static final int MIN_LENGTH_OF_ARRAY = 2;
+
+    private NestChecker() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static int findMin(int[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        return Arrays.stream(array).min().getAsInt();
+    }
+
+    private static int findMax(int[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        return Arrays.stream(array).max().getAsInt();
+    }
+
+    public static boolean isNestable(int[] array1, int[] array2) {
+        if (array1.length < MIN_LENGTH_OF_ARRAY || array2.length < MIN_LENGTH_OF_ARRAY) {
+            throw new IllegalArgumentException();
+        }
+        return (findMin(array1) > findMin(array2)) && (findMax(array1) < findMax(array2));
+    }
+}
---
+++ src/test/java/edu/hw1/task3/NestCheckerTest.java
@@ -0,0 +1,47 @@
+package edu.hw1.task3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class NestCheckerTest {
+
+    @ParameterizedTest
+    @DisplayName("Check the possibility of nesting for VALID arrays")
+    @MethodSource("provideValidParameters")
+    public void testParseValidVideoLength(int[] array1, int[] array2, boolean expectedResult) {
+        final boolean isNestable = NestChecker.isNestable(array1, array2);
+        assertThat(isNestable)
+            .isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideValidParameters() {
+        return Stream.of(
+            Arguments.of(new int[]{1, 2, 3, 4}, new int[]{0, 6}, true),
+            Arguments.of(new int[]{3, 1}, new int[]{4, 0}, true),
+            Arguments.of(new int[]{9, 9, 8}, new int[]{8, 9}, false),
+            Arguments.of(new int[]{1, 2, 3, 4}, new int[]{2, 3}, false)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Check the possibility of nesting for INVALID arrays")
+    @MethodSource("provideInvalidParameters")
+    public void testParseInvalidVideoLength(int[] array1, int[] array2) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            NestChecker.isNestable(array1, array2);
+        });
+    }
+
+    private static Stream<Arguments> provideInvalidParameters() {
+        return Stream.of(
+            Arguments.of(new int[]{1}, new int[]{0, 5}),
+            Arguments.of(new int[]{}, new int[]{0, 6, 9}),
+            Arguments.of(new int[]{}, new int[]{})
+        );
+    }
+}
