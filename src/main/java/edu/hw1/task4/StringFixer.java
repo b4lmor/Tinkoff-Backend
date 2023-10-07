b19d6434210dc595769dcf30b7f0212a2694593b
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-10-07 12:17:22 +0700

    task 4 : String fixer + tests

---
+++ src/main/java/edu/hw1/task4/StringFixer.java
@@ -0,0 +1,29 @@
+package edu.hw1.task4;
+
+public class StringFixer {
+
+    private StringFixer() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String fixString(String brokenString) {
+        if (brokenString.length() <= 1) {
+            return brokenString;
+        }
+
+        StringBuilder fixedString = new StringBuilder();
+
+        for (int i = 1; i < brokenString.length(); i += 2) {
+            char firstLetter = brokenString.charAt(i - 1);
+            char secondLetter = brokenString.charAt(i);
+            fixedString.append(secondLetter);
+            fixedString.append(firstLetter);
+        }
+
+        if (brokenString.length() % 2 != 0) {
+            fixedString.append(brokenString.charAt(brokenString.length() - 1));
+        }
+
+        return fixedString.toString();
+    }
+}
---
+++ src/test/java/edu/hw1/task4/StringFixerTest.java
@@ -0,0 +1,34 @@
+package edu.hw1.task4;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class StringFixerTest {
+
+    @ParameterizedTest
+    @DisplayName("Fix broken string by swapping adjacent letters")
+    @MethodSource("provideParameters")
+    public void testDigitCounter(String brokenString, String expectedResult) {
+        final String fixedString = StringFixer.fixString(brokenString);
+        assertThat(fixedString)
+            .isEqualTo(expectedResult);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+            Arguments.of("123456", "214365"),
+            Arguments.of("hTsii  s aimex dpus rtni.g", "This is a mixed up string."),
+            Arguments.of("badce", "abcde"),
+            Arguments.of("оПомигети псаривьтс ртко!и", "Помогите исправить строки!"),
+            Arguments.of("123456", "214365"),
+            Arguments.of("", ""),
+            Arguments.of("a", "a"),
+            Arguments.of("ab", "ba"),
+            Arguments.of("abc", "bac")
+        );
+    }
+}
