e90cfac6b6d40297250e166f8328d92b1d04f0d0
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-10-12 17:48:27 +0700

    task 2 : Rectangle and Square (LSP)

--- src/main/java/edu/hw2/task2/Rectangle.java
+++ src/main/java/edu/hw2/task2/Rectangle.java
@@ -1,2 +1,20 @@
-package edu.hw2.task2;public class Rectangle {
+package edu.hw2.task2;
+
+public class Rectangle {
+    protected int width = 0;
+    protected int height = 0;
+
+    Rectangle setWidth(int width) {
+        this.width = width;
+        return this;
+    }
+
+    Rectangle setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+
+    double calculateArea() {
+        return width * height;
+    }
 }
--- src/main/java/edu/hw2/task2/Square.java
+++ src/main/java/edu/hw2/task2/Square.java
@@ -1,2 +1,35 @@
-package edu.hw2.task2;public class Square {
+package edu.hw2.task2;
+
+public class Square extends Rectangle {
+    @Override
+    Rectangle setWidth(int w) {
+        if (w == this.height) {
+            return this;
+        }
+        return (new Rectangle())
+            .setWidth(w)
+            .setHeight(this.height);
+    }
+
+    @Override
+    Rectangle setHeight(int h) {
+        if (h == this.width) {
+            return this;
+        }
+        return (new Rectangle())
+            .setHeight(h)
+            .setWidth(this.width);
+    }
+
+    void resize(int side) {
+        this.height = side;
+        this.width = side;
+    }
+
+    public Square() {
+    }
+
+    public Square(int side) {
+        resize(side);
+    }
 }
