79f2f21b20a8b77ff3d4ddb9f44387c726e125de
Artem Lisitsin <artem.lisitsin.02@mail.ru>
2023-12-16 22:26:20 +0700

    task2 : fix

--- src/test/java/edu/hw11/task2/ArithmeticUtils.java
+++ src/test/java/edu/hw11/task2/ArithmeticUtils.java
@@ -1,8 +1,10 @@
 package edu.hw11.task2;
 
 public class ArithmeticUtils {
+    private ArithmeticUtils() {
+    }
 
-    public int sum(int a, int b) {
+    public static int sum(int a, int b) {
         return a + b;
     }
 }
--- src/test/java/edu/hw11/task2/ByteBuddyDynamicClassEditorTest.java
+++ src/test/java/edu/hw11/task2/ByteBuddyDynamicClassEditorTest.java
@@ -1,7 +1,8 @@
 package edu.hw11.task2;
 
+import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.BeforeAll;
@@ -18,23 +19,26 @@ public class ByteBuddyDynamicClassEditorTest {
     @Test
     public void testByteBuddyDynamicClassEditor() {
 
-        new AgentBuilder.Default()
-            .type(ElementMatchers.nameContains("ArithmeticUtils"))
-            .transform((builder, typeDescription, classLoader, module, protectionDomain) ->
-                builder.method(ElementMatchers.named("sum"))
-                    .intercept(MethodDelegation.to(new Object() {
-                        public int sum(int a, int b) {
-                            return a * b;
-                        }
-                    })))
-            .installOnByteBuddyAgent();
+        new ByteBuddy()
+            .redefine(ArithmeticUtils.class)
+            .method(ElementMatchers.named("sum")).intercept(MethodDelegation.to(
+                RedefinedArithmeticUtils.class
+            ))
+            .make()
+            .load(
+                ClassLoader.getSystemClassLoader(),
+                ClassReloadingStrategy.fromInstalledAgent()
+            )
+            .getLoaded();
 
 
-        ArithmeticUtils arithmeticUtils = new ArithmeticUtils();
-        int result = arithmeticUtils.sum(20, 3);
-
+        int result = ArithmeticUtils.sum(20, 3);
         assertEquals(60, result);
     }
 
-
+    private class RedefinedArithmeticUtils {
+        public static int sum(int a, int b) {
+            return a * b;
+        }
+    }
 }
